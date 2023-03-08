a60dd4d41455ce144d5b2127f8a1ae6c4a71cd6f
toninews <toninews@hotmail.com>
2023-03-08 20:08:07 -0300

    corrigindos arquivos para flake8

--- agenda_evento/base/tests/test_home.py
+++ agenda_evento/base/tests/test_home.py
@@ -1,7 +1,10 @@
 from django.test import Client
+
+
 def test_home_status(client: Client):
     response = client.get('/base')
     assert response.status_code == 200
+
 
 def test_home_mensagem(client: Client):
     response = client.get('/base')
