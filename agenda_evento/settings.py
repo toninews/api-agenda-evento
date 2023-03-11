bc62acf8644bb3935cb804763f7e6fd8dd0e5ec3
toninews <toninews@hotmail.com>
2023-03-11 18:40:33 -0300

    incluindo desacoplamento de base de dados por ambiente

--- agenda_evento/settings.py
+++ agenda_evento/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 import os.path
 from pathlib import Path
 import dj_database_url
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -75,7 +76,7 @@ WSGI_APPLICATION = 'agenda_evento.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 default_db_url = 'sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3')
-parser_database = dj_database_url.parse('postgres://toninenews:passtoninenews@localhost:5439/postgres')
+parser_database = dj_database_url.parse(config('DATABASE_URL', default=default_db_url))
 
 DATABASES = {
     'default': parser_database
