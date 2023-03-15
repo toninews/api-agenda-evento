8f633599b532f64d9b817029a16a579d62848e65
toninews <toninews@hotmail.com>
2023-03-15 12:32:14 -0300

    atualizando função de cadastro de evento

--- agenda_evento/api/views.py
+++ agenda_evento/api/views.py
@@ -1,5 +1,6 @@
 from rest_framework.viewsets import GenericViewSet
 from rest_framework.response import Response
+from rest_framework import status
 
 from agenda_evento.api.models import Evento
 from agenda_evento.api.serializers import EventoSerializer
@@ -10,13 +11,17 @@ class EventoViews(GenericViewSet):
     serializer_class = EventoSerializer
 
     def create(self, request):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+
         dados_response = {
-            'mensagem': 'Dados recebidos com sucesso',
-            'dados': request.data
+            'evento_id': serializer.data['id'],
+            'mensagem': f'Evento {serializer.data["titulo"]} criando com sucesso.'
         }
-        return Response(dados_response, 200)
+        return Response(dados_response, status.HTTP_201_CREATED)
 
     def list(self, request):
         queryset = Evento.objects.all()
         serializer = self.get_serializer(queryset, many=True)
-        return Response(serializer.data, 200)
+        return Response(serializer.data, status.HTTP_200_OK)
