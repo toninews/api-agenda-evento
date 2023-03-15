ada3ec89881fb121b9241224a78233e34ab16a95
toninews <toninews@hotmail.com>
2023-03-15 11:48:13 -0300

    criando serialização

---
+++ agenda_evento/api/serializers.py
@@ -0,0 +1,8 @@
+from rest_framework.serializers import ModelSerializer
+
+from agenda_evento.api.models import Evento
+class EventoSerializer(ModelSerializer):
+
+    class Meta:
+        model = Evento
+        fields = '__all__'
